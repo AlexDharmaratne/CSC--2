1e8d636577825a3822de0304f87fa376003c1750
AlexDharma <84358454+AlexDharmaratne@users.noreply.github.com>
2021-06-09 21:55:46 +0000

    I added my Questions to my question dictionary

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from tkinter import *
 import random
 
 names_list = []
-
 global questions_answers
 asked = []
+score = 0
 
 
 # Dictionary has key of number (for each question and number) and : the value for each is a list that has 7 items, so index 0 to 6
@@ -24,11 +24,61 @@ questions_answers = {
     3: ["Which of the following guns cost less than $3000?"
         'AK-47',
         'AWP',
-        ''
-       
-       ] 
-   
+        'M4A4',
+        'SCAR-20',
+        'AK-47'
+        ,1],
+    4: ["How many grenades are there in CSGO?",
+        '2',
+        '7',
+        '5',
+        '6',
+        '6'  
+        ,4],
+    5: ["What does CSGO Stand for?",
+        'Counter Strike Global Offense',
+        'Conflicting Strike GLobal OFfenseive',
+        'Counter Strike Global Offensive',
+        'Conflicting Strike General Offensive',
+        'Counter Strike Global Offensive'
+        ,4],
+    6:["How long does it take to defuse the bomb with a defuse kit?",
+       '3 Seconds',
+       '5 Seconds',
+       '10 Seconds',
+       '7 Seconds',
+       '5 Seconds'
+       ,2],
+    7:["How many pistols are there in CSGO?",
+       '6',
+       '10',
+       '9',
+       '4',
+       '9'
+       ,3],
+    8:["How long does it take to plant the bomb in CSGO",
+       '5 Seconds',
+       '2.5 Seconds',
+       '4 Seconds',
+       '6.5 Seconds',
+       '4 Seconds'
+       ,3],
+    9:["When was CSGO Published?",
+       '21 August 2012',
+       '13 April 2005',
+       '15 October 2008',
+       '14 February 2002'
+       '21 August 2012'
+       ,1],
+    10:["Whats the maximum amount of rounds played in a competitive match",
+        '20',
+        '30',
+        '15',
+        '25',
+        '30'
+        ,2],    
 }
+
 def randomiser():
     global qnum
     qnum = random.randint(1,10)
@@ -67,11 +117,42 @@ class QuizStarter:
       names_list.append(name)
       print(names_list)
       self.quiz_frame.destroy()
+      Quiz(root)
+
+
+class Quiz:
+
+    def __init__(self, parent):
+      background_color = "grey30"
+      self.quiz_frame = Frame(parent, bg = background_color, padx = 40, pady = 40)
+      self.quiz_frame.grid()
+
+      self.question_label = Label (self.quiz_frame, text = questions_answers[qnum][0], font = ("Tw cen MT", "15", "bold"), bg = background_color)
+      self.question_label.grid(row = 0)
+
+      self.var1=IntVar()
+
+      self.rb1= Radiobutton(self.quiz_frame, text=questions_answers[qnum][1], font=("Helvetica", "12"), bg=background_color, value=1,padx=3,pady=3,                 variable=self.var1,   background = "grey")
+      self.rb1.grid(row=2, sticky=W+E)
+
+      self.rb2= Radiobutton(self.quiz_frame, text=questions_answers[qnum][2], font=("Helvetica", "12"), bg=background_color, value=2,padx=3,pady=3,                 variable=self.var1,   background = "grey")
+      self.rb2.grid(row=3, sticky=W+E)
+
+      self.rb3= Radiobutton(self.quiz_frame, text=questions_answers[qnum][3], font=("Helvetica", "12"), bg=background_color, value=3,padx=3,pady=3,                 variable=self.var1,   background = "grey")
+      self.rb3.grid(row=4, sticky=W+E)
+
+      self.rb4= Radiobutton(self.quiz_frame, text=questions_answers[qnum][4], font=("Helvetica", "12"), bg=background_color, value=4,padx=3,pady=3,                 variable=self.var1,   background = "grey")
+      self.rb4.grid(row=5, sticky=W+E)
+
+      self.quiz_instance= Button(self.quiz_frame, text="Confirm", font=("Helvetica", "13", "bold"), bg="grey")
+      self.quiz_instance.grid(row=7, padx=5, pady=5)
+
+      self.score_label=Label(self.quiz_frame, text="Score", font=("Tw Cen MT","16"), bg=background_color,)
+      self.score_label.grid(row=8, padx=10, pady=1)
+
+
       
     
-
-
-
 
 
 
